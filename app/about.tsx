0c60c3ff27fdfc49f1c7b4c8d26e9c4f23a44d8a
sat-lino-au <alberto.gruning.zen@gmai.com>
2025-02-11 06:01:44 +0000

    feat: add images for assets of the app

---
+++ app/about.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>🙈 Developed by QiMono. https://GitHub.com/qimono-repos/</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#253030',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    color:'white',
+    fontSize:20,
+  }
+});
