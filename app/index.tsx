186314c98078989f31cf10df10927f2d2539bb82
sat-lino-au <alberto.gruning.zen@gmai.com>
2025-02-11 07:14:41 +0000

    feat:add navigation for about screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>🙈 Say Hello to QiMono !!!</Text>
+      <Link href={"/about"} style={styles.button}>
+        This project is LGBTQ FOSS friendly. Link to the Repository 
+      </Link>
     </View>
   );
 }
@@ -18,5 +22,12 @@ const styles = StyleSheet.create({
   text:{
     color:'white',
     fontSize:20,
+  },
+  button:{
+    margin:15,
+    fontSize:10,
+    fontStyle:"italic",
+    textDecorationLine:"underline",
+    color:"pink"
   }
 });
