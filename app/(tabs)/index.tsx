6860f2716e4fe1b0c82fde8379592371fca25d8f
sat-lino-au <alberto.gruning.zen@gmai.com>
2025-02-13 08:09:11 +0000

    ref: add component to decouple + re-use image element

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { Image } from 'expo-image'
+import { StyleSheet, View } from 'react-native'
+import ImageViewer from '@/components/imageViewer';
 
 const PlaceholderImage = require('../../assets/images/background-image.png')
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Image source={PlaceholderImage} style={styles.image}/>
-      <Text style={styles.text}>🙈 Say Hello to QiMono !!!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage}/> 
+      </View>
     </View>
   );
 }
@@ -17,21 +18,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#253030',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  text:{
-    color:'white',
-    fontSize:20,
-  },
-  button:{
-    fontSize:20,
-    textDecorationLine:'underline', 
-    color:'#fff',
-  },
-  image:{
-    width:320,
-    height:440,
-    borderRadius:20,
-  },
+  imageContainer: { 
+    flex: 1,
+  }, 
 })
-
---
+++ components/imageViewer.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native'
+import { Image } from 'expo-image'
+
+type Props = { imgSource: string }
+
+export default function ImageViewer({ imgSource }: Props){
+  return <Image source={imgSource} style={styles.image} />
+}
+
+const styles = StyleSheet.create({
+  image:{
+    width:333,
+    height:444,
+    borderRadius:50,
+  },
+})
