df65e008a3cd01083c449c9406c76c19c450d9ca
sat-lino-au <alberto.gruning.zen@gmai.com>
2025-02-14 06:03:06 +0000

    feat: add package to import file from gallery

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,29 @@
 import { StyleSheet, View } from 'react-native'
 import ImageViewer from '@/components/imageViewer'
 import ButtonComponent from '@/components/button'
+import * as ImagePicker from 'expo-image-picker'
 
 const PlaceholderImage = require('../../assets/images/background-image.png')
 
 export default function Index() {
+
+  const pickImageAsync = async () => {
+    let result:any = await ImagePicker.launchImageLibraryAsync({
+      quality:1,
+      allowsEditing: true,
+    })
+    if (!result.cancelled) {
+      console.log(result)
+    } else { alert('No image selected') } 
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage}/> 
       </View>
       <View style={styles.footerContainer}> 
-        <ButtonComponent label='Choose a photo from Gallery' theme='primary'/>
+        <ButtonComponent label='Choose a photo from Gallery' theme='primary' onPress={pickImageAsync}/>
         <ButtonComponent label='Use this photo'/>
       </View>
     </View>
--- components/button.tsx
+++ components/button.tsx
@@ -1,13 +1,11 @@
 import { StyleSheet, View, Pressable, Text } from 'react-native'
 import { FontAwesome } from '@expo/vector-icons'
 
-type Props = {label: string , theme?: 'primary'}
+type Props = {label: string , theme?: 'primary', onPress?: ()=>void}
 
-const onPress = ()=> alert('button press')
-
-const ButtonComponent = ({label, theme }: Props)=>{
+const ButtonComponent = ({label, theme, onPress}: Props)=>{
   if(theme === 'primary') return (
-  <View style={[styles.buttonContainer, {borderWidth: 4, borderColor:'#324054', borderRadius: 25 }]}> 
+  <View style={[styles.buttonContainer, primarybuttonContainer]}> 
     <Pressable style={[styles.pressable, {backgroundColor: '#8e8ea4'}]}  onPress={onPress}>
       <FontAwesome name="picture-o" size={18} color="#252525" style={styles.icon}/>
       <Text style={[styles.textLabel,{color:'#252525'}]}>{label}</Text>
@@ -22,6 +20,7 @@ const ButtonComponent = ({label, theme }: Props)=>{
   </View>
 )}
 
+const primarybuttonContainer = {  borderWidth: 4, borderColor:'#324054', borderRadius: 25 }
 const styles = StyleSheet.create({
   buttonContainer:{
     width:333,
