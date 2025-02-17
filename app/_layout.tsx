3d6c46ad9b8978f00209118549ae4f9cf58488c3
sat-lino-au <alberto.gruning.zen@gmai.com>
2025-02-17 15:51:16 +0000

    feat: import reanimated for gestures

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,17 @@
-import { Stack } from 'expo-router';
-import { LogBox } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { Stack } from 'expo-router'
+import { LogBox } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 //LogBox.ignoreAllLogs(true)
 
 export default function RootLayout() {
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar style='light' />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown:false, }} />
         <Stack.Screen name='not-found' options={{}} />
       </Stack>
-    </>
+    </GestureHandlerRootView>
   )}
--- components/emojiSticker.tsx
+++ components/emojiSticker.tsx
@@ -1,12 +1,33 @@
-import  {View} from 'react-native' 
+import  {ImageSourcePropType, View, StyleSheet} from 'react-native' 
 import {Image} from 'expo-image' 
+import Animated, {useAnimatedStyle, useSharedValue, withSpring} from 'react-native-reanimated' 
+import { Gesture, GestureDetector } from 'react-native-gesture-handler'
 
 type Props = { imagesize: number, source: string }
 
 export const EmojiSticker = ({ imagesize, source }: Props) => {
+  const scaleImage = useSharedValue(imagesize)
+
+  const doubleTap = Gesture
+    .Tap()
+    .numberOfTaps(2)
+    .onStart(() => {
+      if (scaleImage.value == imagesize * 2) scaleImage.value = Math.round(scaleImage.value /2)
+      else scaleImage.value *= 2
+      })
+
+  const imageStyle = useAnimatedStyle(
+    () => {return { width: withSpring(scaleImage.value), height: withSpring(scaleImage.value) }}
+  )
   return (
-    <View style={{top: -350}}>
-      <Image style={{ width: imagesize, height: imagesize }} source={source} />
+    <View style={{top: -350, left: '50%'}}>
+      <GestureDetector gesture={doubleTap}>
+      <Animated.Image 
+        source={source as ImageSourcePropType} 
+        resizeMode={'contain'}
+        style={[imageStyle, { width: imagesize, height: imagesize }]} 
+      />
+      </GestureDetector>
     </View>
   )}
 
