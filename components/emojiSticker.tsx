60ab2f00d9a35fa483e58b362e3553d4befca45a
sat-lino-au <alberto.gruning.zen@gmai.com>
2025-02-17 16:42:58 +0000

    feat: import gesture detector to add drag motion

--- components/emojiSticker.tsx
+++ components/emojiSticker.tsx
@@ -6,6 +6,9 @@ import { Gesture, GestureDetector } from 'react-native-gesture-handler'
 type Props = { imagesize: number, source: string }
 
 export const EmojiSticker = ({ imagesize, source }: Props) => {
+  const translateX = useSharedValue(0)
+  const translateY = useSharedValue(0)
+
   const scaleImage = useSharedValue(imagesize)
 
   const doubleTap = Gesture
@@ -19,16 +22,26 @@ export const EmojiSticker = ({ imagesize, source }: Props) => {
   const imageStyle = useAnimatedStyle(
     () => {return { width: withSpring(scaleImage.value), height: withSpring(scaleImage.value) }}
   )
+  const drag = Gesture
+    .Pan()
+    .onChange((event)=>{ translateX.value = event.changeX; translateY.value = event.changeY; })
+
+  const containerStyle = useAnimatedStyle(()=> {
+    return { transform: [{ translateX: translateX.value }, { translateY: translateY.value }] }
+  })
+
   return (
-    <View style={{top: -350, left: '50%'}}>
-      <GestureDetector gesture={doubleTap}>
-      <Animated.Image 
-        source={source as ImageSourcePropType} 
-        resizeMode={'contain'}
-        style={[imageStyle, { width: imagesize, height: imagesize }]} 
-      />
-      </GestureDetector>
-    </View>
+    <GestureDetector gesture={drag}>
+      <Animated.View style={[containerStyle,{top: -350, left: '50%'}]}>
+        <GestureDetector gesture={doubleTap}>
+        <Animated.Image 
+          source={source as ImageSourcePropType} 
+          resizeMode={'contain'}
+          style={[imageStyle, { width: imagesize, height: imagesize }]} 
+        />
+        </GestureDetector>
+      </Animated.View>    
+    </GestureDetector>
   )}
 
 // export default EmojiSticker
